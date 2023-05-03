7582ad14dc5be6f95df12322e9c0b5c54741e9da
Agustin cilli <cilli.agustin@gmail.com>
2023-05-03 09:05:51 +0000

    Add style to create raing form

--- src/pages/movies/MoviePage.js
+++ src/pages/movies/MoviePage.js
@@ -9,10 +9,15 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Movie from "./Movie";
+import RatingCreateForm from "../ratings/RatingCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function MoviePage() {
     const { id } = useParams()
     const [movie, setMovie] = useState({results: []})
+    const currentUser = useCurrentUser()
+    const profile_image = currentUser?.profile_image;
+    const profile_id = currentUser?.profile_id;
 
     useEffect(()=>{
         const handleMount = async ()=>{
@@ -21,7 +26,6 @@ function MoviePage() {
                     axiosReq.get(`/movies/${id}`)
                 ])
                 setMovie({results: [movie]})
-                console.log(movie)
             } catch (err) {
                 console.log(err)
             }
@@ -35,8 +39,8 @@ function MoviePage() {
     <Row className="h-100 mx-0">
       <Col className="p-0">
         <Movie {...movie.results[0]} setMovies={setMovie} />
-        <Container className={appStyles.Content}>
-          Comments
+        <Container fluid className={`${appStyles.Content} mx-0 px-0`}>
+          <RatingCreateForm profile_id={profile_id} profile_image={profile_image} />
         </Container>
       </Col>
     </Row>
--- src/pages/ratings/RatingCreateForm.js
+++ src/pages/ratings/RatingCreateForm.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../../styles/RatingCreateEditForm.module.css'
+import btnStyles from '../../styles/Button.module.css'
+import appStyles from '../../App.module.css'
 
 import { Link } from "react-router-dom";
 
@@ -7,23 +9,47 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 import Avatar from "../../components/Avatar";
-import { axiosRes } from "../../api/axiosDefaults";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function RatingCreateForm(props) {
-  const { movie, setMovie, setRatings, profileImage, profile_id } = props;
-  const [content, setContent] = useState("");
+  const { movie, setMovie, setRatings, profile_image, profile_id } = props;
+  const [ratingData, setRatingData] = useState({
+        title: "",
+        content: "",
+        value: "",
+        movieData: movie,
+    });
 
-  const handleChange = (event) => {
-    setContent(event.target.value);
-  };
+    const { title, content, value, movieData } = ratingData;
+
+    const handleChange = (event) => {
+        setRatingData({
+            ...ratingData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleClick = (e)=> {
+      e.preventDefault()
+      setRatingData({
+        ...ratingData,
+        value: e.target.value,
+      });
+      console.log(value)
+    }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const formData = new FormData();
+
+    formData.append('title', title);
+    formData.append('content', content);
+    formData.append('value', value);
+    formData.append('movie', movie);
+
+
     try {
-      const { data } = await axiosRes.post("/ratings/", {
-        content,
-        movie,
-      });
+        const { data } = await axiosReq.post("/ratings/", formData);
       setRatings((prevRating) => ({
         ...prevRating,
         results: [data, ...prevRating.results],
@@ -36,31 +62,78 @@ function RatingCreateForm(props) {
           },
         ],
       }));
-      setContent("");
     } catch (err) {
       console.log(err);
     }
   };
 
+
   return (
-    <Form className="mt-2" onSubmit={handleSubmit}>
+    <Form className={styles.CreateRating} onSubmit={handleSubmit}>
       <Form.Group>
-        <InputGroup>
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profileImage} />
-          </Link>
+        <InputGroup className={styles.FormDisplay}>
+          <Avatar 
+              src={profile_image} 
+              height={55}
+              id={profile_id}
+              username={null}
+              className={styles.Avatar}
+          />
           <Form.Control
-            className={styles.Form}
-            placeholder="my comment..."
+            className={`w-100 ${styles.Input}`}
+            placeholder='Review Title'
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange}
+          />
+          <div className={styles.ButtonControl}>
+            <button
+              className={`${styles.StarButton} ${value < 1 ? styles.Grey : appStyles.Green}`}
+              onClick={(e) => {handleClick(e);}} value={1}>
+                <i className="fa-solid fa-star"></i>
+            </button>
+            <button
+              className={`${styles.StarButton} ${value < 2 ? styles.Grey : appStyles.Green}`}
+              onClick={(e) => {handleClick(e);}} value={2}>
+                <i className="fa-solid fa-star"></i>
+            </button>
+            <button
+              className={`${styles.StarButton} ${value < 3 ? styles.Grey : appStyles.Green}`}
+              onClick={(e) => {handleClick(e);}} value={3}>
+                <i className="fa-solid fa-star"></i>
+            </button>
+            <button
+              className={`${styles.StarButton} ${value < 4 ? styles.Grey : appStyles.Green}`}
+              onClick={(e) => {handleClick(e);}} value={4}>
+                <i className="fa-solid fa-star"></i>
+            </button>
+            <button
+              className={`${styles.StarButton} ${value < 5 ? styles.Grey : appStyles.Green}`}
+              onClick={(e) => {handleClick(e);}} value={5}>
+                <i className="fa-solid fa-star"></i>
+            </button>
+          </div>
+          <Form.Control
+                    className={`d-none`}
+                    type="number"
+                    name="value"
+                    value={value}
+                    readOnly
+                />
+          <Form.Control
+            className={`w-100 ${styles.Textarea}`}
+            placeholder='Review Title'
             as="textarea"
+            rows={6}
+            name="content"
             value={content}
             onChange={handleChange}
-            rows={2}
           />
         </InputGroup>
       </Form.Group>
       <button
-        className={`${styles.Button} btn d-block ml-auto`}
+        className={`mx-auto mt-4 ${btnStyles.Button}`}
         disabled={!content.trim()}
         type="submit"
       >
