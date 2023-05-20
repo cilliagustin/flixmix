cf02f995bcc477ee85fc4419ebae4a552c2e4c26
Agustin cilli <cilli.agustin@gmail.com>
2023-05-20 13:49:49 +0000

    Add conditional display to follow and unfollow buttons

--- src/pages/profiles/ProfilePreviewCard.js
+++ src/pages/profiles/ProfilePreviewCard.js
@@ -3,14 +3,16 @@ import styles from '../../styles/ProfilePreviewCard.module.css'
 import BtnStyles from '../../styles/Button.module.css'
 import Avatar from '../../components/Avatar'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
 
 const ProfilePreviewCard = (props) => {
-  console.log(props)
+  const currentUser = useCurrentUser()
   const {
     followers_count, following_count, following_id, id, name, owner, image,
     is_owner, list_count, movie_count, seen_count, watchlist_count, rating_count,
     setProfiles
   } = props
+  console.log(following_id)
   return (
     <div className={styles.Container}>
       <div className={styles.CardHead}>
@@ -29,8 +31,12 @@ const ProfilePreviewCard = (props) => {
           <span>{owner}</span>
           {name !== "" && <><br /><span>({name})</span></>}
         </Link>
-        {!is_owner && (
-          <button className={`${BtnStyles.Button} ${styles.Button}`}>Follow</button>
+        {currentUser && !is_owner && (
+          following_id ? (
+            <button className={`${BtnStyles.Button} ${BtnStyles.White} ${styles.Button}`}>Unfollow</button>
+          ) : (
+            <button className={`${BtnStyles.Button} ${BtnStyles.HoverWhite} ${styles.Button}`}>Follow</button>
+          )
         )}
         <p className={styles.Follow}>
           <span>Following</span><br />{following_count}
@@ -40,26 +46,26 @@ const ProfilePreviewCard = (props) => {
         </p>
       </div>
       <div className={styles.CardBody}>
-          <div className={styles.Info}>
-            <span>Seen Movies:</span>
-            <span>{seen_count}</span>
-          </div>
-          <div className={styles.Info}>
-            <span>Movies in Watchlist:</span>
-            <span>{watchlist_count}</span>
-          </div>
-          <div className={styles.Info}>
-            <span>Movies added:</span>
-            <span>{movie_count}</span>
-          </div>
-          <div className={styles.Info}>
-            <span>Reviews Written:</span>
-            <span>{rating_count}</span>
-          </div>
-          <div className={styles.Info}>
-            <span>Lists Created:</span>
-            <span>{list_count}</span>
-          </div>
+        <div className={styles.Info}>
+          <span>Seen Movies:</span>
+          <span>{seen_count}</span>
+        </div>
+        <div className={styles.Info}>
+          <span>Movies in Watchlist:</span>
+          <span>{watchlist_count}</span>
+        </div>
+        <div className={styles.Info}>
+          <span>Movies added:</span>
+          <span>{movie_count}</span>
+        </div>
+        <div className={styles.Info}>
+          <span>Reviews Written:</span>
+          <span>{rating_count}</span>
+        </div>
+        <div className={styles.Info}>
+          <span>Lists Created:</span>
+          <span>{list_count}</span>
+        </div>
       </div>
     </div>
   )
